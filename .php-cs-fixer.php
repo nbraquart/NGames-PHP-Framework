40a19367030cf8b9920bdf1043c4d90f7c4c823c
Nicolas BRAQUART <nicolas.braquart@gmail.com>
2021-10-21 23:54:29 +0100

    PHP CS fixer config

---
+++ .php-cs-fixer.php
@@ -0,0 +1,6 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()->in(__DIR__)->exclude('vendor');
+
+$config = new PhpCsFixer\Config();
+return $config->setRules(['@PSR12' => true])->setFinder($finder);
--- .php_cs
+++
@@ -1,5 +0,0 @@
-<?php
-
-$finder = PhpCsFixer\Finder::create()->in(__DIR__)->exclude('vendor');
-
-return PhpCsFixer\Config::create()->setFinder($finder);
